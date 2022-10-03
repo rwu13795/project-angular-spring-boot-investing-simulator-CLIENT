fe14a9cf40e3c7037e1bb1f26e602ed8a6e1bef3
Ray <rwu13795@gmail.com>
2022-10-03 12:39:36 -0400

    found a way to implement the delayed input search

--- src/app/digit-slide/digit-slide.component.ts
+++ src/app/digit-slide/digit-slide.component.ts
@@ -14,6 +14,8 @@ import {
 export class DigitSlideComponent implements OnInit, OnChanges {
   @Input() digit: number = 0;
 
+  inputTimer?: any;
+
   ngOnInit(): void {
     console.log(this.digit);
   }
@@ -29,5 +31,20 @@ export class DigitSlideComponent implements OnInit, OnChanges {
 
       slide.style.transform = `translateY(${slideDistance * newNum}px)`;
     }
+  }
+
+  onInputChange(event: any) {
+    let input = event.target as HTMLInputElement;
+
+    if (this.inputTimer) clearTimeout(this.inputTimer);
+
+    // after each user input change, set a 800ms timer to wait and see if user
+    // is still entering input. If there is new input, the old timer will be
+    // cleared and new timer will be created. If there is no input change
+    // after 800ms, then trigger the callback (send http request and etc...)
+    this.inputTimer = setTimeout(() => {
+      console.log("input stop:", input.value);
+      // send http request to make the search
+    }, 800);
   }
 }
