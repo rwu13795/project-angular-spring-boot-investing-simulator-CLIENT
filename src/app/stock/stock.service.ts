9985b618716b0a8b80ce5573a252116d69e32a71
Ray <rwu13795@gmail.com>
2022-10-10 01:33:45 -0400

    added 3M 6M 1Y time range for chart

--- src/app/stock/stock.service.ts
+++ src/app/stock/stock.service.ts
@@ -20,6 +20,11 @@ export interface Response_historyPrice {
   volume: number;
 }
 
+interface Response_historyPriceFull {
+  // for the daily price api "/historical-price-full"
+  historical: Response_historyPrice[];
+}
+
 export interface CandleData {
   x: Date; // timestamp
   y: number[]; // [open, high, low, close]
@@ -37,12 +42,13 @@ export interface ChartData {
   lowBound: number;
 }
 
-interface FetchedData {
+interface StoredData {
   ["5D"]: ChartData | null;
   ["1M"]: ChartData | null;
   ["3M"]: ChartData | null;
   ["6M"]: ChartData | null;
   ["1Y"]: ChartData | null;
+  [range: string]: ChartData | null;
 }
 
 @Injectable({ providedIn: "root" })
@@ -54,7 +60,7 @@ export class StockService {
 
   private API_KEY = "bebf0264afd8447938b0ae54509c1513";
 
-  private storedData: FetchedData = {
+  private storedData: StoredData = {
     ["5D"]: null,
     ["1M"]: null,
     ["3M"]: null,
@@ -99,107 +105,101 @@ export class StockService {
     // new Date().getUTCHours() - 4
 
     const { from, to, timeRange, interval } = this.getTimeRange(option);
-
     const params = new HttpParams({
       fromObject: { from, to, apikey: this.API_KEY },
     });
 
-    return this.http
-      .get<Response_historyPrice[]>(
-        `${this.FMP_API}/historical-chart/${timeRange}/AAPL`,
-        { params }
-      )
-      .pipe(
-        map<Response_historyPrice[], ChartData>((responseData) => {
-          const data: ChartData = {
-            volumns: [],
-            candles: [],
-            candleLine: [],
-            highBound: -1,
-            lowBound: Infinity,
-          };
-
-          // put some "placeholders" at the start of the arrays
-          if (option === "1D") {
-            const firstEntryTimestamp =
-              responseData[responseData.length - 1].date;
-            for (let i = 6; i >= 1; i--) {
-              const timestamp = new Date(
-                new Date(firstEntryTimestamp).getTime() - interval * i
-              );
-              data.candles.push({
-                x: timestamp,
-                y: [-1],
-              });
-              data.volumns.push({ x: timestamp, y: 0 });
-            }
-          }
-
-          for (let i = responseData.length - 1; i >= 0; i--) {
-            const { date, open, high, low, close, volume } = responseData[i];
-
-            // get the price range
-            if (high * 1.002 > data.highBound) {
-              data.highBound = high * 1.002;
-            }
-            if (low * 0.998 < data.lowBound) {
-              data.lowBound = low * 0.998;
-            }
-
-            // map the data into seperate arrays for the charts
-            data.candles.push({
-              x: new Date(date),
-              y: [open, high, low, close],
-            });
-            data.volumns.push({ x: new Date(date), y: volume });
-            // use the "close" price for the line chart
-            data.candleLine.push({ x: new Date(date), y: close });
-          }
-
-          // put some "placeholders" at the end of the arrays
-          if (option === "1D") {
-            const lastEntryTimestamp = responseData[0].date;
-            for (let i = 1; i <= 6; i++) {
-              const timestamp = new Date(
-                new Date(lastEntryTimestamp).getTime() + interval * i
-              );
-              data.candles.push({
-                x: timestamp,
-                y: [-1],
-              });
-              data.volumns.push({ x: timestamp, y: 0 });
-            }
-          }
-
-          switch (option) {
-            case "5D": {
-              this.storedData["5D"] = data;
-              break;
-            }
-            case "1M": {
-              this.storedData["1M"] = data;
-              break;
-            }
-            // case "3M": {
-            //   break;
-            // }
-            // case "6M": {
-            //   break;
-            // }
-            // case "1Y": {
-            //   break;
-            // }
-            default:
-              break;
-          }
-
-          return data;
+    let apiUrl = `${this.FMP_API}/historical-chart/${timeRange}/AAPL`;
+    if (timeRange === "") {
+      apiUrl = `${this.FMP_API}/historical-price-full/AAPL`;
+      return this.http.get<Response_historyPriceFull>(apiUrl, { params }).pipe(
+        map<Response_historyPriceFull, ChartData>((responseData) => {
+          return this.mapResponseData(
+            responseData.historical,
+            option,
+            interval
+          );
         })
       );
+    }
+    return this.http.get<Response_historyPrice[]>(apiUrl, { params }).pipe(
+      map<Response_historyPrice[], ChartData>((responseData) => {
+        return this.mapResponseData(responseData, option, interval);
+      })
+    );
   }
 
-  getStoredDate(option: "5D" | "1M" | "3M" | "6M" | "1Y") {
+  public getStoredDate(option: "5D" | "1M" | "3M" | "6M" | "1Y") {
     return this.storedData[option];
+  }
+
+  private mapResponseData(
+    responseData: Response_historyPrice[],
+    option: string,
+    interval: number
+  ): ChartData {
+    const data: ChartData = {
+      volumns: [],
+      candles: [],
+      candleLine: [],
+      highBound: -1,
+      lowBound: Infinity,
+    };
+
+    // put some "placeholders" at the start of the arrays
+    if (option === "1D") {
+      const firstEntryTimestamp = responseData[responseData.length - 1].date;
+      for (let i = 6; i >= 1; i--) {
+        const timestamp = new Date(
+          new Date(firstEntryTimestamp).getTime() - interval * i
+        );
+        data.candles.push({
+          x: timestamp,
+          y: [-1],
+        });
+        data.volumns.push({ x: timestamp, y: 0 });
+      }
+    }
+
+    for (let i = responseData.length - 1; i >= 0; i--) {
+      const { date, open, high, low, close, volume } = responseData[i];
+
+      // get the price range
+      if (high * 1.002 > data.highBound) {
+        data.highBound = high * 1.002;
+      }
+      if (low * 0.998 < data.lowBound) {
+        data.lowBound = low * 0.998;
+      }
+
+      // map the data into seperate arrays for the charts
+      data.candles.push({
+        x: new Date(date),
+        y: [open, high, low, close],
+      });
+      data.volumns.push({ x: new Date(date), y: volume });
+      // use the "close" price for the line chart
+      data.candleLine.push({ x: new Date(date), y: close });
+    }
+
+    // put some "placeholders" at the end of the arrays
+    if (option === "1D") {
+      const lastEntryTimestamp = responseData[0].date;
+      for (let i = 1; i <= 6; i++) {
+        const timestamp = new Date(
+          new Date(lastEntryTimestamp).getTime() + interval * i
+        );
+        data.candles.push({
+          x: timestamp,
+          y: [-1],
+        });
+        data.volumns.push({ x: timestamp, y: 0 });
+      }
+    }
+
+    this.storedData[option] = data;
+
+    return data;
   }
 
   private getTimeRange(option: string): {
@@ -238,15 +238,27 @@ export class StockService {
         interval = 60000 * 60;
         break;
       }
-      // case "3M": {
-      //   break;
-      // }
-      // case "6M": {
-      //   break;
-      // }
-      // case "1Y": {
-      //   break;
-      // }
+      case "3M": {
+        from_date = new Date(to_date.getTime() - 86400000 * 90);
+        from_date = this.fromWeekendsToMonday(from_date);
+        timeRange = "4hour";
+        interval = 60000 * 60 * 4;
+        break;
+      }
+      case "6M": {
+        from_date = new Date(to_date.getTime() - 86400000 * 180);
+        from_date = this.fromWeekendsToMonday(from_date);
+        timeRange = "";
+        interval = 60000 * 60 * 24;
+        break;
+      }
+      case "1Y": {
+        from_date = new Date(to_date.getTime() - 86400000 * 365);
+        from_date = this.fromWeekendsToMonday(from_date);
+        timeRange = "";
+        interval = 60000 * 60 * 24;
+        break;
+      }
       default: {
         // default is "1D"
         from_date = to_date;
