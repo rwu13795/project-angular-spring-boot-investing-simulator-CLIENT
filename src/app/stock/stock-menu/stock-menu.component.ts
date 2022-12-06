f15537b750cdb9e37151e887e8f2a5d72234f2c6
Ray <rwu13795@gmail.com>
2022-12-06 16:05:28 -0500

    found the bug in stock-menu, fixed the bug, need more time to test it during market hour

--- src/app/stock/stock-menu/stock-menu.component.ts
+++ src/app/stock/stock-menu/stock-menu.component.ts
@@ -24,6 +24,7 @@ import { setPreviousSymbol } from "../stock-state/stock.actions";
 })
 export class StockMenuComponent implements OnInit, OnDestroy {
   private symbol$?: Subscription;
+  private previousSymbol$?: Subscription;
   private assets$?: Subscription;
   private activeMenu$?: Subscription;
   private hasAuth$?: Subscription;
@@ -51,23 +52,48 @@ export class StockMenuComponent implements OnInit, OnDestroy {
       }
     });
 
-    this.store
-      .select(selectPreviousSymbol)
-      .pipe(take(1))
-      .subscribe((previousSymbol) => {
-        this.symbol$ = this.store
-          .select(selectCurrentSymbol)
-          .subscribe((currentSymbol) => {
-            if (currentSymbol === previousSymbol) return;
-
+    this.symbol$ = this.store
+      .select(selectCurrentSymbol)
+      .subscribe((currentSymbol) => {
+        console.log("currentSymbol", currentSymbol);
+        this.store
+          .select(selectPreviousSymbol)
+          .pipe(take(1))
+          .subscribe((previousSymbol) => {
+            console.log(
+              "currentSymbol",
+              currentSymbol,
+              "previousSymbol",
+              previousSymbol
+            );
+            console.log("this.symbol", this.symbol);
+            if (previousSymbol === currentSymbol) return;
             this.symbol = currentSymbol;
-            this.store.dispatch(setPreviousSymbol({ symbol: currentSymbol }));
+            this.store.dispatch(setPreviousSymbol({ symbol: this.symbol }));
 
-            if (currentSymbol && currentSymbol !== "") {
+            console.log("this.symbol", this.symbol);
+
+            if (this.symbol && this.symbol !== "") {
+              // fetch the latest price in every 20s if the current menu is not "chart"
+              if (this.stockService.isMarketOpen()) {
+                clearInterval(this.updateTimer);
+                this.updateTimer = setInterval(() => {
+                  this.updatePrice();
+                }, 1000 * 20);
+              }
+
+              this.assets$ = this.store
+                .select(selectAssets)
+                .subscribe((assets) => {
+                  if (assets) this.hasAsset = !!assets[this.symbol];
+                });
+
               // after getting the symbol from store
               this.activeMenu$ = this.store
                 .select(selectStockActiveMenu)
                 .subscribe((menu) => {
+                  console.log("menu", menu);
+
                   this.activeMenu = menu;
                   this.showButton = this.activeMenu === StockMenu.chart;
 
@@ -77,26 +103,13 @@ export class StockMenuComponent implements OnInit, OnDestroy {
                   // update the price whenever the menu is changed
                   this.updatePrice();
 
-                  // fetch the latest price in every 20s if the current menu is not "chart"
                   if (
-                    this.activeMenu !== StockMenu.chart &&
+                    this.activeMenu === StockMenu.chart &&
                     this.stockService.isMarketOpen()
                   ) {
-                    clearInterval(this.updateTimer);
-                    this.updateTimer = setInterval(() => {
-                      this.updatePrice();
-                    }, 1000 * 20);
-                  }
-
-                  if (this.activeMenu === StockMenu.chart) {
+                    // clear the 20s interval, let the real-time-chart update the price
                     clearInterval(this.updateTimer);
                   }
-                });
-
-              this.assets$ = this.store
-                .select(selectAssets)
-                .subscribe((assets) => {
-                  if (assets) this.hasAsset = !!assets[currentSymbol];
                 });
             }
           });
@@ -113,6 +126,8 @@ export class StockMenuComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     clearInterval(this.updateTimer);
+
+    if (this.previousSymbol$) this.previousSymbol$.unsubscribe();
     if (this.symbol$) this.symbol$.unsubscribe();
     if (this.activeMenu$) this.activeMenu$.unsubscribe();
     if (this.assets$) this.assets$.unsubscribe();
@@ -121,7 +136,20 @@ export class StockMenuComponent implements OnInit, OnDestroy {
 }
 
 /*
+--------- (1) ---------
 
+I have to store the previous symbol in the stock state. When the stock menu
+is mounted and there is a "currentSymbol" in the stock state, and the selector
+select the "currentSymbol" on init, the new "currentSymbol" might not have been
+set (in the stock-guard), so the selector might select the "old" symbol.
+
+This will cause the issue where an updatePrice interval is using the the "old"
+symbol. Also, when the selector detect the "update" of the "currentSymbol", it
+wiil select the "currentSymbol" again, then another updatePrice interval will be
+created using the updated "currentSymbol"
+
+I need to check if (currentSymbol === previousSymbol), if it is true, then it 
+means the seleceted "currentSymbol" is not current
 
 
 */
